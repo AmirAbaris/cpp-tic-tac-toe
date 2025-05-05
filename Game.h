2632b501f8290c86681777ef0e145a847e37e2b5
Amir Abaris <thisisamirabaris@gmail.com>
2025-05-05 17:45:26 +0330

    feat: draw the board logic implemented

--- Game.h
+++ Game.h
@@ -3,6 +3,15 @@
 
 class Game
 {
+public:
+    static const int rows = 3;
+    static const int cols = 3;
+    char board[rows][cols];
+    char playerX = 'X';
+    char playerO = 'O';
+
+    void Init();
+    void DrawBoard();
 };
 
 #endif
